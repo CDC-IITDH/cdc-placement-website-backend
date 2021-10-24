05606901929650afe1a3d7804af6a11cf828501d
CDC-IITDH <76999613+CDC-IITDH@users.noreply.github.com>
2021-10-24 21:00:30 +0530

    Update admin.py

--- CDC_Backend/APIs/admin.py
+++ CDC_Backend/APIs/admin.py
@@ -3,10 +3,7 @@ from .models import *
 
 admin.site.register(User)
 admin.site.register(Student)
-admin.site.register(PR)
-admin.site.register(Company)
+admin.site.register(Admin)
 admin.site.register(Placement)
-admin.site.register(Internship)
 admin.site.register(PlacementApplication)
-admin.site.register(InternshipApplication)
 admin.site.register(PrePlacementOffer)
