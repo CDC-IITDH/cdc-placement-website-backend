a05ec1430f876803ca995880494ab87a6ece11f6
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-06-06 19:03:04 +0530

    creating db command added

--- setup.bat
+++ setup.bat
@@ -2,11 +2,14 @@
 start /wait  python -m venv venv && venv\Scripts\activate && pip install -r requirements.txt &^
 echo Environment Setup Complete &^
 timeout 3 > NUL &^
+echo enter password for user postgres &^
+createdb -h localhost -p 5432 -U postgres cdc &^
 cd "CDC_Backend" &^
 
 python manage.py flush --no-input &^
 python manage.py makemigrations &^
 python manage.py migrate &^
 echo Migrations complete &^
-start superuser.bat &^
+cd .. &^
+start .\superuser.bat &^
 echo done successfully
