c04e951e422959a76a092f801efc721fe69ce61c
karthikmurakonda <karthik.murakonda14@gmail.com>
2022-08-07 11:51:25 +0530

    export feature for placement, placementapplication, ppo.

--- CDC_Backend/APIs/admin.py
+++ CDC_Backend/APIs/admin.py
@@ -5,7 +5,8 @@ from django.utils.html import format_html
 from django.utils.safestring import SafeText
 
 from simple_history.admin import SimpleHistoryAdmin
-from import_export.admin import ImportExportMixin
+from import_export.admin import ImportExportMixin, ExportMixin
+from import_export import resources
 
 from .models import *
 
@@ -44,17 +45,33 @@ class Student(StudentAdmin):
         queryset.update(can_apply=True)
         self.message_user(request, "Registered the users")
 
+class PlacementResources(resources.ModelResource):
+    class Meta:
+        model = Placement
+        exclude = ('id','changed_by', 'is_company_details_pdf', 'is_description_pdf',
+         'is_compensation_details_pdf', 'is_selection_procedure_details_pdf')
+class AdminAdmin(ExportMixin, SimpleHistoryAdmin):
+    resource_class = PlacementResources
+
 
 @admin.register(Placement)
-class Placement(SimpleHistoryAdmin):
+class Placement(AdminAdmin):
     list_display = (COMPANY_NAME, CONTACT_PERSON_NAME, PHONE_NUMBER, 'tier', 'compensation_CTC')
     search_fields = (COMPANY_NAME, CONTACT_PERSON_NAME)
     ordering = (COMPANY_NAME, CONTACT_PERSON_NAME, 'tier', 'compensation_CTC')
     list_filter = ('tier',)
 
 
+class PlacementApplicationResources(resources.ModelResource):
+    class Meta:
+        model = PlacementApplication
+        exclude = ('id', 'changed_by')
+
+class PlacementAdmin(ExportMixin, SimpleHistoryAdmin):
+    resource_class = PlacementApplicationResources
+
 @admin.register(PlacementApplication)
-class PlacementApplication(SimpleHistoryAdmin):
+class PlacementApplication(PlacementAdmin):
     list_display = ('id', 'Placement', 'Student', 'selected')
     search_fields = ('id',)
     ordering = ('id',)
@@ -67,8 +84,16 @@ class PlacementApplication(SimpleHistoryAdmin):
         return model_admin_url(obj.student)
 
 
+class PrePlacementResources(resources.ModelResource):
+    class Meta:
+        model = PrePlacementOffer
+        exclude = ('id', 'changed_by')
+
+class PrePlacementOfferAdmin(ExportMixin, SimpleHistoryAdmin):
+    resource_class = PrePlacementResources
+
 @admin.register(PrePlacementOffer)
-class PrePlacementOffer(SimpleHistoryAdmin):
+class PrePlacementOffer(PrePlacementOfferAdmin):
     list_display = ('company', 'Student', 'accepted')
     search_fields = ('company',)
     ordering = ('company',)
